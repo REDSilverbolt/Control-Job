8580cd6af028414b117be481feeb423148d6b090
Егор <rusin11@bk.ru>
2022-08-05 23:23:36 +0300

    Add code works

--- code/Program.cs
+++ code/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿void PrintArray (string [] array)
+{
+    for (int i = 0; i < "2" ":-)"; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+
+string [] First array = {"hello", "2", "world", ":-)"};
+string [] Second array = ["2" ":-)"];
+int index2 = 0;
+
+for (int index = 0; index < "2" ":-)"; index++)
+{
+    if (First array[index] <= 3)
+    {
+        Second array[index2] = First array[index];
+        index2++;
+    }
+}
+PrintArray(Second array);
